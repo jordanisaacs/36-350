185d3e4d9434b27d6c1fda6596fe37b70506b442
jdisa <jdisa@JISAACS-LAPTOP>
2020-07-30 14:26:27 -0700

    Updated Message

--- hello_aliens.r
+++ hello_aliens.r
@@ -3,4 +3,6 @@ cat("What's up aliens. Hope yall are peaceful. With how its going on earth you\n
     fusion power, vaccines, or something it would be great if you could to send it all over.\n
     Could be quite useful right about now as our planet ain't doing so well. \n
     But don't send us any weapons or anything because we got enough of those :( \n
-    Any help is appreciated!")
+    Any help is appreciated!\n\n
+    Signed your poor celestial neighbor,\n
+    Earth")

d590e179d68c88c245bc5de3e60cded1156b6cf9
jdisa <jdisa@JISAACS-LAPTOP>
2020-07-30 14:22:20 -0700

    Wrote message

---
+++ hello_aliens.r
@@ -0,0 +1,6 @@
+cat("What's up aliens. Hope yall are peaceful. With how its going on earth you\n
+    probably won't be rip. But anyways if you got some crazy technologies like\n
+    fusion power, vaccines, or something it would be great if you could to send it all over.\n
+    Could be quite useful right about now as our planet ain't doing so well. \n
+    But don't send us any weapons or anything because we got enough of those :( \n
+    Any help is appreciated!")
